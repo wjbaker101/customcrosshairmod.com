a0af7a81ebf5be163e0b217f2df58a36d764bf1a
William Baker <wjbaker101@users.noreply.github.com>
2025-05-10 23:10:24 +0100

    Add supported mod loaders

--- components/sections/LandingSectionComponent.vue
+++ components/sections/LandingSectionComponent.vue
@@ -4,7 +4,7 @@
             <h1 class="mb-2 text-3xl font-bold">Custom Crosshair Mod</h1>
             <p class="mb-4"><small>{{ topVersions.join(' , ') }} + more!</small></p>
             <p class="mb-2">DOWNLOAD:</p>
-            <div class="space-x-2">
+            <div class="mb-4 space-x-2">
                 <a href="https://www.curseforge.com/minecraft/mc-mods/custom-crosshair-mod" target="_blank">
                     <ButtonComponent class="text-black !bg-curseforge hover:!bg-curseforge-light focus:!outline-curseforge">
                         <CurseForgeIcon class="mr-2 inline-block align-middle" />
@@ -18,6 +18,7 @@
                     </ButtonComponent>
                 </a>
             </div>
+            <p class="mb-4">Supports: <strong>{{ loaders.join(', ') }}</strong></p>
             <CrosshairComponent class="my-12" />
             <SupportComponent />
         </div>
@@ -39,5 +40,11 @@ const topVersions = [
     '1.12',
     '1.8.9',
     '1.7.10',
+];
+
+const loaders = [
+    'Fabric',
+    'Forge',
+    'NeoForge',
 ];
 </script>
