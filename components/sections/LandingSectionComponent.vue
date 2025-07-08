3069478617cde1b18850585d509ddf67f43ebc1e
William Baker <wjbaker101@users.noreply.github.com>
2025-07-08 20:13:10 +0100

    New latest version

--- components/sections/LandingSectionComponent.vue
+++ components/sections/LandingSectionComponent.vue
@@ -34,7 +34,7 @@ import { ArrowDownIcon } from 'lucide-vue-next';
 import { CurseForgeIcon, ModrinthIcon } from 'vue3-simple-icons';
 
 const topVersions = [
-    '1.21.5',
+    '1.21.7',
     '1.18.2',
     '1.16.5',
     '1.12',
