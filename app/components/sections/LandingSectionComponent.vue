084f2d803d1c573f504379700f3e3bd766fa4d2e
William Baker <wjbaker101@users.noreply.github.com>
2025-08-08 11:33:13 +0100

    Update latest version

--- app/components/sections/LandingSectionComponent.vue
+++ app/components/sections/LandingSectionComponent.vue
@@ -1,19 +1,19 @@
 <template>
-    <div class="relative grid h-screen place-items-center">
+    <div class="relative place-items-center grid h-screen">
         <div class="text-center">
-            <h1 class="mb-2 text-3xl font-bold">Custom Crosshair Mod</h1>
+            <h1 class="mb-2 font-bold text-3xl">Custom Crosshair Mod</h1>
             <p class="mb-4"><small>{{ topVersions.join(' , ') }} + more!</small></p>
             <p class="mb-2">DOWNLOAD:</p>
-            <div class="mb-4 space-x-2">
+            <div class="space-x-2 mb-4">
                 <a href="https://www.curseforge.com/minecraft/mc-mods/custom-crosshair-mod" target="_blank">
                     <ButtonComponent class="text-black !bg-(--curseforge) hover:!bg-(00curseforge-light) focus:!outline-(--curseforge)">
-                        <CurseForgeIcon class="mr-2 inline-block align-middle" />
+                        <CurseForgeIcon class="inline-block mr-2 align-middle" />
                         <span class="align-middle">CurseForge</span>
                     </ButtonComponent>
                 </a>
                 <a href="https://modrinth.com/mod/custom-crosshair-mod" target="_blank">
                     <ButtonComponent class="!bg-(--modrinth) hover:!bg-(--modrinth-dark) focus:!outline-(--modrinth)">
-                        <ModrinthIcon class="mr-2 inline-block align-middle" />
+                        <ModrinthIcon class="inline-block mr-2 align-middle" />
                         <span class="align-middle">Modrinth</span>
                     </ButtonComponent>
                 </a>
@@ -22,7 +22,7 @@
             <CrosshairComponent class="my-12" />
             <SupportComponent />
         </div>
-        <div class="absolute bottom-2 text-center">
+        <div class="bottom-2 absolute text-center">
             <p>See more below!</p>
             <ArrowDownIcon class="inline-block animate-bounce" />
         </div>
@@ -34,7 +34,7 @@ import { ArrowDownIcon } from 'lucide-vue-next';
 import { CurseForgeIcon, ModrinthIcon } from 'vue3-simple-icons';
 
 const topVersions = [
-    '1.21.7',
+    '1.21.8',
     '1.18.2',
     '1.16.5',
     '1.12',
