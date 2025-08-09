253c418edd6dddac33083090ed6c381dc2f909b1
William Baker <wjbaker101@users.noreply.github.com>
2025-08-09 19:18:03 +0100

    Add the latest diff

--- app/components/pages/stats/ProjectLineGraph.vue
+++ app/components/pages/stats/ProjectLineGraph.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="my-8">
-        <h2 class="mb-4 text-xl text-center">{{ Mapper.retriever(data.retrieverIdentifier) }} {{ data.dataType}}</h2>
+        <h2 class="mb-2 text-xl text-center">{{ Mapper.retriever(data.retrieverIdentifier) }} {{ data.dataType}}</h2>
+        <div class="mb-4 text-sm text-center">
+            <strong class="font-bold">Latest: </strong>
+            <span>{{ formatNumber(data.values.at(-1)!.value) }}</span>
+            (<span
+                :class="{
+                    'text-green-600': latestDiff > 0,
+                    'text-red-600': latestDiff < 0,
+                }"
+            >{{ latestDiffFormatted }}</span>)
+        </div>
         <LineChart
             :data="dataToDisplay"
             :categories="categories"
@@ -40,4 +50,18 @@ const categories = {
 const xAxisFormatter = (i: number) => dataToDisplay.value[i]!.retrievedAt.format('DD/MM/YYYY');
 
 const xAxisLabel = computed(() => `Date (${dataToDisplay.value[0]?.retrievedAt.fromNow(true)} span)`);
+
+const latestDiff = computed(() => {
+    return data.values.at(-1)!.value - data.values.at(-2)!.value;
+});
+
+const latestDiffFormatted = computed(() => {
+    const latest = formatNumber(Math.abs(latestDiff.value));
+
+    if (latestDiff.value < 0) {
+        return `-${latest}`;
+    }
+
+    return `+${latest}`;
+});
 </script>
