e0796a179d489bdcbfdc39dec56400bb1951775a
William Baker <wjbaker101@users.noreply.github.com>
2025-10-12 18:11:53 +0100

    Imgur blocks the UK/VPNs

--- app/components/sections/FeaturesSectionComponent.vue
+++ app/components/sections/FeaturesSectionComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="features-section">
-        <h1 class="my-12 text-center text-4xl">Features</h1>
+        <h1 class="my-12 text-4xl text-center">Features</h1>
         <ol>
             <li>Change size, colour, shape!</li>
             <li>
@@ -12,11 +12,11 @@
             <li>Draw your own crosshair!</li>
             <li>5 fully-supported languages! (English/Spanish/German/Chinese/Icelandic)</li>
         </ol>
-        <h2 class="my-8 text-center text-2xl">NEW FEATURE: Draw your own crosshair!</h2>
+        <h2 class="my-8 text-2xl text-center">NEW FEATURE: Draw your own crosshair!</h2>
         <img
             width="854"
             height="480"
-            src="https://i.imgur.com/X1bajgW.png"
+            src="https://cdn.modrinth.com/data/o1tyE5vJ/images/73aba3c4758c1eeeed3127f1d15de853e1f388f9.png"
             alt="Image showcasing the unique crosshairs possible when drawing crosshairs."
             title="NEW FEATURE: Draw your own crosshair!"
             class="mx-auto rounded-md"
